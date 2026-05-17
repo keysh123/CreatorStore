0c02f57948ec189be48e8d2e2bd262f97cc43904
keysh123 <keyashah126@gmail.com>
2026-05-18 01:50:05 +0200

    Initial commit

---
+++ CreatorStore/src/test/java/com/example/CreatorStore/CreatorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreatorStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
