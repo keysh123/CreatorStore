02d33263381a32febba1d8324364fc213fbb3972
keysh123 <keyashah126@gmail.com>
2026-05-18 02:42:18 +0200

    setting up of dotenv for application

--- CreatorStore/src/main/java/com/example/CreatorStore/CreatorStoreApplication.java
+++ CreatorStore/src/main/java/com/example/CreatorStore/CreatorStoreApplication.java
@@ -1,12 +1,17 @@
 package com.example.CreatorStore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CreatorStoreApplication {
 
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("./CreatorStore").ignoreIfMissing().load();
+		dotenv.entries().forEach((entry)-> System.setProperty(entry.getKey(),entry.getValue()));
+		System.out.println("DATABASE_URL = " + System.getProperty("DATABASE_URL"));
 		SpringApplication.run(CreatorStoreApplication.class, args);
 	}
 
